7a374cfa58394d1ca60a5d17bcb4ce70c6013533
nicgirault <nic.girault@gmail.com>
2018-01-12 16:50:18 +0100

    move animation in App

--- src/index.js
+++ src/index.js
@@ -1,46 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import io from 'socket.io-client';
-import anime from 'animejs';
-
-const socket = io(process.env.NODE_ENV === 'production' ? 'https://speech.lalilo.com/shhht' : 'http://localhost:5000')
-
-export const record = () => {
-  return navigator.mediaDevices.getUserMedia({
-    audio: {
-      sampleSize: 16,
-      channelCount: 1,
-      sampleRate: 16000
-    }
-  })
-  .then(stream => {
-    const mediaRecorder = new MediaRecorder(stream)
-    mediaRecorder.start(250)
-    mediaRecorder.ondataavailable = (event) => {
-      socket.emit('message', new Blob([event.data], { 'type' : 'audio/ogg; codecs=opus' }))
-    }
-  })
-}
-
-record()
-
-
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-const animation = anime({
-  targets: '#lineDrawing .lines path',
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'easeInOutSine',
-  duration: 1500,
-  delay: function(el, i) { return i * 500 },
-});
-
-socket.on('level', (level) => {
-  if (level > 1.5) {
-    return animation.pause()
-  }
-  animation.play()
-})
