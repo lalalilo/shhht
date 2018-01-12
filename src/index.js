7aa2e63589396c1f973886fe04450a6500a1e0c0
nicgirault <nic.girault@gmail.com>
2018-01-12 12:25:12 +0100

    record on start

--- src/index.js
+++ src/index.js
@@ -4,5 +4,24 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+export const record = () => {
+  return navigator.mediaDevices.getUserMedia({
+    audio: {
+      sampleSize: 16,
+      channelCount: 1,
+      sampleRate: 16000
+    }
+  })
+  .then(stream => {
+    const mediaRecorder = new MediaRecorder(stream)
+    mediaRecorder.start(100)
+    mediaRecorder.ondataavailable = (event) => {
+      console.log('coucou')
+    }
+  })
+}
+
+record()
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
