dc6909127ee5f5318315be14fc255b1373570f7d
nicgirault <nic.girault@gmail.com>
2018-01-20 11:36:35 +0100

    remove speech api for this project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Logo from './logo';
 import styled from 'styled-components';
-import io from 'socket.io-client';
 import SetupPage from './SetupPage';
 import IllustrationPage from './IllustrationPage';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import { colors } from './levels'
+import { colors, getLevel } from './levels'
 
 const Wrapper = styled.div`
 width: 100%;
@@ -22,12 +21,10 @@ const muiTheme = getMuiTheme({
   },
 });
 
-const socket = io(process.env.NODE_ENV === 'production' ? 'https://speech.lalilo.com/shhht' : 'http://localhost:5000/shhht')
-
 class App extends React.Component {
   state = {
     color: colors[0],
-    level: 1,
+    levelThreshold: 1,
     isStarted: false,
     isPlaying: false,
     duration: 60000 * 55
@@ -43,31 +40,46 @@ class App extends React.Component {
       }
     })
     .then(stream => {
-      const mediaRecorder = new MediaRecorder(stream)
       this.stream = stream
-      mediaRecorder.start(250)
-      mediaRecorder.ondataavailable = (event) => {
-        socket.emit('message', new Blob([event.data], { 'type' : 'audio/ogg; codecs=opus' }))
+      const context = new AudioContext()
+      this.context = context
+      const analyser = context.createAnalyser()
+      analyser.smoothingTimeConstant = 0.3
+      analyser.fftSize = 1024
+
+      var audioInput = context.createMediaStreamSource(stream)
+
+      const javascriptNode = context.createScriptProcessor(2048 * 8, 1, 1)
+      // connect to destination, else it isn't called
+      javascriptNode.connect(context.destination)
+
+      // connect the source to the analyser
+      audioInput.connect(analyser)
+
+      // we use the javascript node to draw at a specific interval.
+      analyser.connect(javascriptNode)
+
+      javascriptNode.onaudioprocess = () => {
+        const array =  new Uint8Array(analyser.frequencyBinCount)
+        analyser.getByteFrequencyData(array)
+
+        const level = getLevel(array)
+        this.setState({
+          color: colors[level],
+          isPlaying: level <= this.state.levelThreshold
+        })
       }
-    })
-
-    socket.on('level', (level) => {
-      console.log('level', level)
-      const color = colors[Math.round(level)] || colors[0]
-
-      this.setState({
-        color: color,
-        isPlaying: level <= this.state.level
-      })
     })
   }
 
   end = () => {
     this.stream.getTracks()[0].stop()
+    this.context.close()
   }
 
   reset = () => {
     this.stream.getTracks()[0].stop()
+    this.context.close()
     this.setState({
       isStarted: false,
       isPlaying: false,
@@ -83,10 +95,10 @@ class App extends React.Component {
     if (!this.state.isStarted) {
       return (
         <SetupPage
-          level={this.state.level}
+          level={this.state.levelThreshold}
           duration={this.state.duration}
           onDurationChange={(newValue) => this.setState({duration: newValue})}
-          onLevelChange={(newValue) => this.setState({level: newValue})}
+          onLevelChange={(newValue) => this.setState({levelThreshold: newValue})}
           onSubmit={this.start}
         />
       )
@@ -95,7 +107,7 @@ class App extends React.Component {
       <IllustrationPage
         duration={this.state.duration}
         color={this.state.color}
-        level={this.state.level}
+        level={this.state.levelThreshold}
         play={this.state.isStarted && this.state.isPlaying}
         reset={this.reset}
         print={this.print}
--- src/levels.js
+++ src/levels.js
@@ -13,3 +13,12 @@ export const colors = {
   3: "#F772B4",
   4: "#F76767",
 }
+
+export const getLevel = (frequencyData) => {
+  const mean = frequencyData.reduce((sum, current) => sum += current, 0) / frequencyData.length
+  if (mean < 10) return 0
+  if (mean < 30) return 1
+  if (mean < 50) return 2
+  if (mean < 70) return 3
+  return 4
+}
